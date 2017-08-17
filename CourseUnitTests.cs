2c54618d7709b2389b466113a30cb2100d09bad3
Nathan Waid <natewaid@hotmail.com>
2017-08-17 08:43:17 -0400

    added arrange/act/assert comments in the test method.

--- CourseUnitTests.cs
+++ CourseUnitTests.cs
@@ -11,14 +11,17 @@
         [TestMethod]
         public void complete_returns_correct_message()
         {
+            //arrange
             var course = new Course();
 
             var mockPerson = new Mock<IPerson>();
 
             mockPerson.Setup(s => s.CompleteClass()).Returns("test");
 
+            //act
             var result = course.Complete(mockPerson.Object);
 
+            //assert
             Assert.AreEqual(result, "test");
         }
     }
